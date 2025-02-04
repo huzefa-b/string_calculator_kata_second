cdf33f52d31b9e931c256adeffc6f6cd25ea79fc
huzefa <huzefa.b@incubyte.co>
2025-02-04 21:52:06 +0530

    Add logic to handle negative numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -8,6 +8,11 @@ class StringCalculator
       delimiter = Regexp.escape(header[2])
     end
 
-    numbers.split(/#{delimiter}/).map(&:to_i).sum
+    parsed_numbers = numbers.split(/#{delimiter}/).map(&:to_i)
+    negatives = parsed_numbers.select { |n| n < 0 }
+
+    raise "negative numbers not allowed: #{negatives.join(', ')}" unless negatives.empty?
+
+    parsed_numbers.sum
   end
 end
