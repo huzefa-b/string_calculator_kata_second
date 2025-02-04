615ed0e3b5865de0861c1d9ce2c142ebbf0ab3c8
huzefa <huzefa.b@incubyte.co>
2025-02-04 21:49:20 +0530

    Add case when input has custom delimiter

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -31,5 +31,11 @@ RSpec.describe StringCalculator do
         expect(StringCalculator.new.add("1\n2,3")).to eq(6)
       end
     end
+
+    context 'when input contains a custom delimiter' do
+      it 'returns the sum' do
+        expect(calculator.add("//;\n1;2")).to eq(3)
+      end
+    end
   end
 end
