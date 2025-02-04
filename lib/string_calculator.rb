1906369baa4e1275073643724309673770857a70
huzefa <huzefa.b@incubyte.co>
2025-02-04 21:50:00 +0530

    Add logic for handling custom delimiter

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,6 +2,12 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
 
-    numbers.split(/,|\n/).map(&:to_i).sum
+    delimiter = ",|\n"
+    if numbers.start_with?("//")
+      header, numbers = numbers.split("\n", 2)
+      delimiter = Regexp.escape(header[2])
+    end
+
+    numbers.split(/#{delimiter}/).map(&:to_i).sum
   end
 end
